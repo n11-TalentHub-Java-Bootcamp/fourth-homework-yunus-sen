605f4b7eedd4d66bd67352f0ac6f0fe9122fed11
YUNUS <yunussen@ycp.com.tr>
2022-01-17 09:44:31 +0300

    initializer project.

---
+++ src/main/java/com/yunussen/debtpaymentws/DebtPaymentWsApplication.java
@@ -0,0 +1,13 @@
+package com.yunussen.debtpaymentws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtPaymentWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebtPaymentWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunussen/debtpaymentws/DebtPaymentWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunussen.debtpaymentws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtPaymentWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
